a9744a6aabcd9998c296cca9db4ce9e0f227283d
myselfshravan <myselfshravan@gmail.com>
2023-09-04 22:16:25 +0530

    last

--- sentimental_analysis.py
+++ sentimental_analysis.py
@@ -12,15 +12,21 @@ class Mood:
 def get_mood(input_text: str, *, threshold: float) -> Mood:
     sentiment: float = TextBlob(input_text).sentiment.polarity
 
-    friendly_threshold: float = threshold
-    hostile_threshold: float = -threshold
-
-    if sentiment >= friendly_threshold:
+    if sentiment > threshold:
         return Mood("😄", sentiment)
-    elif sentiment <= hostile_threshold:
+    elif sentiment < -threshold:
         return Mood("😠", sentiment)
     else:
         return Mood("😐", sentiment)
+
+
+def get_sentiment_label(sentiment: float) -> str:
+    if sentiment > 0:
+        return "positive"
+    elif sentiment < 0:
+        return "negative"
+    else:
+        return "neutral"
 
 
 st.title("Sentiment Analysis Chatbot")
@@ -30,9 +36,28 @@ text = st.chat_input("Enter some text... ")
 if text:
     try:
         mood: Mood = get_mood(text, threshold=0.05)
-        mood_result = f"Mood: {mood.emoji} : {mood.sentiment:.2f}"
+        sentiment_label = get_sentiment_label(mood.sentiment)
+        mood_result = f"Sentiment: {sentiment_label} : {mood.emoji}"
         st.subheader(f"Result for: {text}")
         st.subheader(mood_result)
+        st.subheader(f"Score: {mood.sentiment:.2f}")
 
     except Exception as e:
         st.write(f"An error occurred: {str(e)}")
+
+hide_streamlit_style = """
+                    <style>
+                    # MainMenu {visibility: hidden;}
+                    footer {visibility: hidden;}
+                    footer:after {
+                    content:''; 
+                    visibility: visible;
+    	            display: block;
+    	            position: relative;
+    	            # background-color: red;
+    	            padding: 15px;
+    	            top: 2px;
+    	            }
+                    </style>
+                    """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
